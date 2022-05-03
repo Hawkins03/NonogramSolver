f433ed424994a04775270b647fdb3678a2ab794c
hawins03 <falcor.peterson@gmail.com>
2022-05-03 11:39:38 -0400

    commiting

--- Row.java
+++ Row.java
@@ -6,51 +6,94 @@ import NonogramSolver.*;
  * @version 10.16.21
  */
 public class Row {
-    protected Block[] blocks; //the array of blocks that this array will hold
-    protected double[] probabilities; //the probability that a space will be filled or not
-    protected int[] positions; //an integer version of probabilities 0 = not, 1 = maybe, 2 = guarenteed
-    protected int blockLength = 0; //the sum of all block lengths + the number of blocks - 1
-    protected final int LENGTH; // the total length of the row
+    private final int LENGTH;
+    private int[] blockLengths;
+    private double[] probabilities; //psudo probabilities (0% = -1 and not 0
+    private int blockSum;
+    public Row(int LENGTH, int[] blockLengths) throws ArrayIndexOutOfBoundsException{
 
-    public Row(int LENGTH, int[] blockLengths) {
-        blocks = new Block[blockLengths.length];
-        for (int i = 0; i < blockLengths.length; i++) {
-            blocks[i] = new Block(blockLengths[i]);
-            blockLength += blockLengths[i];
+        //is this row valid (ie would the total length of the blocks be < the row length
+        int sum = 0;
+        for(int i : blockLengths)
+            sum += i;
+        int blockSum = sum + blockLengths.length - 1;
+
+        if(blockSum > LENGTH) {
+            throw new ArrayIndexOutOfBoundsException("Blocks total to " + blockSum + 
+                    " but the length of the row is " + LENGTH);
         }
-        blockLength += blockLengths.length - 1;
 
+        //row is legit and can be instantiated
         this.LENGTH = LENGTH;
-        probabilities = new double[LENGTH + 2];
-        positions = new int[LENGTH + 2];
+        this.blockSum = blockSum;
+        probabilities = new double[LENGTH + 2]; //allowing for edges to occur
+
+        //setting edges to be 0s (not possible for a barrier to occur)
+        probabilities[0] = -1.0;
+        probabilities[-1] = -1.0;
     }
 
-    public void initialSolve() {
-        int guarenteedLength = LENGTH - blockLength;
-        int itterator = 1;
-        for (Block i : blocks) {
-            if (i.LENGTH > guarenteedLength) {
-                i.setPos(itterator + i.LENGTH - guarenteedLength, itterator + i.LENGTH);
-            }
-            // end updating blocks
-            
-            for(int j = itterator; j < itterator + LENGTH - 1; j++) {
-                for (int k = 0; k < i.LENGTH; k++) {
-                    probabilities[j + k] += 1 / (guarenteedLength + 1);
-                }
-            }
-            itterator += i.LENGTH + 1;
+    public void  formatProbabilities() {
+        for (int i = 0; i < probabilities.length; i++) {
+            if (probabilities[i] == 0.0)
+                probabilities[i] = -1.0;
+            else if (Math.abs(probabilities[i]) == 1.00)
+                continue;
+            else
+                probabilities[i] = 0.0;
         }
+    }
 
-        for (int i = 0; i < 11; i ++) {
-            if (probabilities[i] == 1.0) {
-                positions[i] = 2;
-            } else if (probabilities[i] > 0.0 && probabilities[i] < 1.0) {
-                positions[i] = 1;
-            } else if (probabilities[i] == 0.0) {
-                positions[i] = 0;
-            } else {
-                positions[i] = -1;
+    public void probabilityAssesment() {
+        int itterator = 0;
+        for (int i : blockLengths) {
+            for (int j = itterator; j < itterator + i + 1; j++) {
+                boolean xFound = false;
+                for (int k = 0; k < i; k++) {
+                    if (itterator == 0 && probabilities[j + k] == 0.0)
+                        itterator = j + k;
+                    else if (itterator == 1) {
+                        xFound = true;
+                        break;
+                    } else if (itterator != 0)
+                        probabilities[j + k] += 1 / (blockSum + 1);
+                }
+                if (xFound)
+                    break;
+            }
+            itterator += i + 1;
+        }
+    } // need to make sure this works
+
+    public void findFinishedBlocks() {
+        int minPos = 0;
+        int maxPos;
+        for (int i = 0; i < blockLengths.length; i++) {
+            for(int j = 0; j < i; j++)
+                minPos += probabilities[j];
+            minPos++;
+            maxPos = LENGTH + blockLengths[i] - minPos; //recheck math
+
+            for(minPos = minPos; minPos < maxPos; minPos++) {
+                switch (-1 * (int) (probabilities[minPos] - probabilities[minPos + 1])) { //-1 for ordering correctly
+                    case -2: //1.0 followed by -1.0
+                        try {
+                            minPos -= blockLengths[i]; //move back the length of the block
+                        } catch (Exception e) {
+                            throw new ArrayIndexOutOfBoundsException("Error, found block clipping out of row");
+                        }
+                    case 2://-1.0 followed by 1.0
+                        double[] arrayCopy = probabilities;
+                        if (arrayCopy[minPos] != 1 && arrayCopy[maxPos] != 1) {
+                            arrayCopy[minPos] = -1;
+                            arrayCopy[maxPos] = -1;
+                            for (int k = 0; k < arrayCopy.length; k++)
+                                if (Math.abs(arrayCopy[k + minPos]) != -1)
+                                    arrayCopy[k + minPos] = 1;
+
+                        }
+
+                }
             }
         }
     }
